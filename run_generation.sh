921b36e0c8f3fe417aeb69024bd35033b93363b3
Xingjian Shi <xshiab@connect.ust.hk>
2021-06-21 23:36:13 -0700

    Update run_generation.sh

--- run_generation.sh
+++ run_generation.sh
@@ -4,7 +4,7 @@ set -ex
 
 python3 generation_example.py \
     --output_dir ./ \
-    --input 'Why AutoGluon is great?' \
+    --input "Why AutoGluon is great?" \
     --max_length 800 \
     --top_p 0.95 \
     --download_dir ./ \
